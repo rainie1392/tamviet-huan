377729706df7ac9349f0f2464cfe3789e3fdeb71
Nguyễn Trung Khánh <m.caine1392@gmail.com>
2015-09-05 11:56:54 +0700

    cont

--- TamVietFE/TamVietFE/Src/Default.aspx.cs
+++ TamVietFE/TamVietFE/Src/Default.aspx.cs
@@ -11,7 +11,13 @@ namespace TamVietFE.Src
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                string aboutUs = "Công ty Cổ phần Y tế Quang Minh được thành lập vào ngày 29/05/2007, trải qua gần 10 năm hoạt động Quang Minh tự hào là nhà cung cấp sản phẩm và dịch vụ Chăm sóc sức khỏe con người hàng đầu tại Việt Nam.Với hệ thống phân phối trải dài từ Bắc vào Nam, Quang Minh đã đánh dấu sự có mặt của mình khắp các tỉnh thành trên cả nước, mang đến cho khách hàng sự hài lòng về các sản phẩm, dịch vụ mà Quang Minh cung cấp với phương châm “ Phát triển vì sức khỏe cộng đồng”  tạo nên giá trị cốt lõi Doanh Nghiệp.";
+                int iNextSpace = aboutUs.LastIndexOf(" ", 250);
+                var temp = string.Format("{0}...", aboutUs.Substring(0, (iNextSpace > 0) ? iNextSpace : 250).Trim());
+                litAboutUs.Text = temp;
+            }
         }
     }
 }
